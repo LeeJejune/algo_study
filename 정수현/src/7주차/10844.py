935a7ecda5e781b267e12fd3ffc8d19a8753b34b
SuHyeon Jeong <119032609+kmujsh@users.noreply.github.com>
2023-03-16 16:56:50 +0900

    [BOJ] 쉬운 계단 수 / 실버1 / X
    https://www.acmicpc.net/problem/10844

---
+++ 정수현/src/7주차/10844.py
@@ -0,0 +1,20 @@
+# [BOJ] 쉬운 계단 수 / 실버1 / X
+# https://www.acmicpc.net/problem/10844
+# https://pacific-ocean.tistory.com/151
+
+N = int(input())
+dp = [[0 for _ in range(10)] for _ in range(101)]
+
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for i in range(2, N + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i - 1][1]
+        elif j == 9:
+            dp[i][j] = dp[i - 1][8]
+        else:
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+
+print(sum(dp[N]) % 1000000000)
